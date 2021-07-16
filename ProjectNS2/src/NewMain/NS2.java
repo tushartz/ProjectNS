b71cd1b1c89f7758cfb366b627d6b6f0e232ecb5
Tushar <tusharzode15198@gmail.com>
2021-07-16 18:59:17 +0530

    Initial Commit

---
+++ ProjectNS2/src/NewMain/NS2.java
@@ -0,0 +1,14 @@
+package NewMain;
+
+public class NS2 {
+	public static void main(String[] args)
+	{
+		String val = "SE";
+		if (val == "SE") {
+		System.out.println("Software Engineer");
+		}
+		else {
+			System.out.println("Not Software Engineer");
+		}
+	}
+}
